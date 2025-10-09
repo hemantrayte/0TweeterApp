95f9162ec6cf5554f1b1f7ce0569eff147494c9e
hemant <hemantrayte25@gmail.com>
2025-10-09 11:33:28 +0530

    define route in app.js file

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,8 +2,13 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("basic setup");
-});
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+//++++++ routes ++++++++///
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
