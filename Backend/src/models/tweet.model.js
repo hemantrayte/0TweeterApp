a0da7f4d7c3edb7cfe1a6fae4fbb9936085cc63b
hemant <hemantrayte25@gmail.com>
2025-10-09 09:23:29 +0530

    add tweet model

---
+++ Backend/src/models/tweet.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const tweetSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Tweet = mongoose.model("Tweet", tweetSchema);
