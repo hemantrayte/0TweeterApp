5751c04a3d3bf4f52a5951b8c98588b0c3698605
hemant <hemantrayte25@gmail.com>
2025-10-10 17:05:51 +0530

    define like routes

--- Backend/src/routes/like.routes.js
+++ Backend/src/routes/like.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
+import { VerifyJwt } from "../middlewares/auth.middleware";
+import {
+  getLikedTweets,
+  toggleCommentLike,
+  toggleTweetLike,
+} from "../controllers/like.controller";
 
 const router = Router();
+router.use(VerifyJwt);
+
+router.route("/toggle/t/:tweetId").post(toggleTweetLike);
+router.route("/toggle/c/:commentId").post(toggleCommentLike);
+
+router.get(getLikedTweets);
 
 export default router;
