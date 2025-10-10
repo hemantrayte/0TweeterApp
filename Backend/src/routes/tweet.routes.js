5304e53814c7092ec83b9a1cb1514ac849e13c84
hemant <hemantrayte25@gmail.com>
2025-10-10 11:20:05 +0530

    tweet update and delete routes

--- Backend/src/routes/tweet.routes.js
+++ Backend/src/routes/tweet.routes.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
 import { VerifyJwt } from "../middlewares/auth.middleware.js";
-import { allTweet, createTweet } from "../controllers/tweet.controller";
+import {
+  allTweet,
+  createTweet,
+  deleteTweet,
+  updateTweet,
+  userTweets,
+} from "../controllers/tweet.controller";
 
 const router = Router();
 router.use(VerifyJwt);
 
 router.route("/").post(createTweet);
 router.route("/").get(allTweet);
+router.route("/user/:userId").get(userTweets);
+router.route("/:tweetId").patch(updateTweet);
+router.route("/:tweetId").patch(deleteTweet);
 
 export default router;
