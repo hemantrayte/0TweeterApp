15aecdc2e150ec7d32ab73de57149d08a144a779
hemant <hemantrayte25@gmail.com>
2025-10-10 17:07:12 +0530

    define likes route in app js file

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,8 +14,11 @@ app.use(express.static("public"));
 import userRouter from "./routes/user.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+
 export { app };
