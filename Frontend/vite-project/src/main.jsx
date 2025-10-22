bd79bbf68cd067ee9758ce0e57075a43c907372f
hemant <hemantrayte25@gmail.com>
2025-10-22 09:42:13 +0530

    set up routing with BrowserRouter and Home component

--- Frontend/vite-project/src/main.jsx
+++ Frontend/vite-project/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <RouterProvider router={router} />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
