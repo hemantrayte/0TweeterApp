7d4fed857fe06a40667830a9bbeb88579a91ca50
hemant <hemantrayte25@gmail.com>
2025-10-22 11:06:01 +0530

    create  subscription routes

--- Backend/src/routes/subscription.routes.js
+++ Backend/src/routes/subscription.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
+import { VerifyJwt } from "../middlewares/auth.middleware.js";
+import {
+  followUser,
+  getFollowers,
+  getFollowing,
+  unfollowUser,
+} from "../controllers/subscription.controller.js";
 
 const router = Router();
+
+router.route("/follow/:userId").post(VerifyJwt, followUser);
+router.route("/unfollow/:userId").delete(VerifyJwt, unfollowUser);
+router.route("/followers/:userId").get(getFollowers);
+router.route("/following/:userId").get(getFollowing);
 
 export default router;
