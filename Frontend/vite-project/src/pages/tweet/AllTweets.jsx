8a8d07a07541dbf475a44a020b1cde913e19d48b
hemant <hemantrayte25@gmail.com>
2025-10-22 11:23:13 +0530

    define Alltweets and Create tweet page

---
+++ Frontend/vite-project/src/pages/tweet/AllTweets.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AllTweets = () => {
+  return (
+    <div>
+      <h1>All Tweets</h1>
+    </div>
+  );
+};
+
+export default AllTweets;
---
+++ Frontend/vite-project/src/pages/tweet/CreateTweet.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateTweet = () => {
+  return (
+    <div>
+      <h1>Create Tweet</h1>
+    </div>
+  );
+};
+
+export default CreateTweet;
