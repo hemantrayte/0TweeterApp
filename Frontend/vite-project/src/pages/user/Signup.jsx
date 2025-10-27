065e6f52db7993633f34693994ddaccceaeb1c64
hemant <hemantrayte25@gmail.com>
2025-10-27 12:17:38 +0530

    install axios

--- Frontend/vite-project/src/pages/user/Signup.jsx
+++ Frontend/vite-project/src/pages/user/Signup.jsx
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Signup = () => {
+  const [userData, setUserData] = useState({
+    email: "",
+    username: "",
+    password: "",
+    fullName: "",
+  });
+
+  const [avatar, setAvatar] = useState(null);
+
+  const handleInputeChange = (e) => {
+    setUserData({
+      ...userData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFileChange = (e) => {
+    if (e.target.name === "avatar") {
+      setAvatar(e.target.file[0]);
+    }
+  };
+
+  const handleSubmit = async () => {};
+
   return (
     <div>
-      <h1>Sign up page</h1>
+      <h1>Sign up User</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            value={userData.email}
+            onChange={handleInputeChange}
+          />
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            value={userData.username}
+            onChange={handleInputeChange}
+          />
+          <input
+            type="text"
+            name="password"
+            placeholder="password"
+            value={userData.password}
+            onChange={handleInputeChange}
+          />
+          <input
+            type="text"
+            name="fullName"
+            placeholder="fullName"
+            value={userData.fullName}
+            onChange={handleInputeChange}
+          />
+          <input
+            type="file"
+            name="avatar"
+            accept="image/*"
+            onChange={handleFileChange}
+            className="w-full text-sm"
+            required
+          />
+        </div>
+      </form>
     </div>
   );
 };
