474622a5f481e3d40f8c9d31e5ad4b04b3207b24
hemant <hemantrayte25@gmail.com>
2025-10-28 14:18:50 +0530

    add updateTweet page

--- Frontend/vite-project/src/pages/tweet/UpdateTweet.jsx
+++ Frontend/vite-project/src/pages/tweet/UpdateTweet.jsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import api from "../../Api/api";
 
 const UpdateTweet = () => {
+  const { tweetId } = useParams();
+
+  const [tweet, setTweet] = useState({
+    content: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleInputeChange = (e) => {
+    setTweet({
+      ...tweet,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const fetchSingleTweet = async () => {
+    try {
+      const res = await api.get(`/tweets/id/${tweetId}`);
+      setTweet(res.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSingleTweet();
+  }, [tweetId]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await api.patch(
+        `/tweets/${tweetId}`,
+        { content: tweet.content },
+        { headers: { "Content-Type": "application/json" } }
+      );
+      // setMessage(res.data.message);
+      console.log(res.data);
+      navigate(`/tweet/${tweetId}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <h1>Update Tweet</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="content"
+          value={tweet.content}
+          onChange={handleInputeChange}
+        />
+        <button>Update Tweet</button>
+      </form>
     </div>
   );
 };
