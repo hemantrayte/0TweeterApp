4d6b5d5c96671b81b6e983e8aa455b0285474310
hemant <hemantrayte25@gmail.com>
2025-10-28 12:14:41 +0530

    Add image upload functionality to CreateTweet component with form handling

--- Frontend/vite-project/src/pages/tweet/CreateTweet.jsx
+++ Frontend/vite-project/src/pages/tweet/CreateTweet.jsx
@@ -1,9 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "../../Api/api";
+import { useNavigate } from "react-router-dom";
 
 const CreateTweet = () => {
+  const [tweet, setTweet] = useState({
+    content: "",
+  });
+
+  const navigate = useNavigate();
+
+  const [image, setImage] = useState(null);
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    setTweet({
+      ...tweet,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFileChange = (e) => {
+    if (e.target.name === "image") {
+      setImage(e.target.files[0]);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = new FormData();
+      data.append("content", tweet.content);
+      data.append("image", image);
+
+      //Use Api helper insted of axios
+      const res = await api.post("/tweets/create", data, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+
+      console.log(res.data.data);
+      setMessage(res.data.message || "Tweet created successfully!");
+
+      setTweet({
+        content: "",
+      });
+
+      navigate("/tweet");
+    } catch (error) {
+      console.log(error);
+      setMessage(error.response?.data?.message || "Something went wrong");
+    }
+  };
+
   return (
     <div>
-      <h1>Create Tweet</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="content"
+          value={tweet.content}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="file"
+          name="image"
+          accept="image/*"
+          onChange={handleFileChange}
+          required
+        />
+        <button type="submit">Create Tweet</button>
+      </form>
     </div>
   );
 };
