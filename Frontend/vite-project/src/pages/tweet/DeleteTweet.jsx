c60a8a92b46d6aea2bd0442f1cf9dd284a7485cd
hemant <hemantrayte25@gmail.com>
2025-10-28 14:30:39 +0530

    add delete tweet page

--- Frontend/vite-project/src/pages/tweet/DeleteTweet.jsx
+++ Frontend/vite-project/src/pages/tweet/DeleteTweet.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-
+import { useNavigate, useParams } from "react-router-dom";
+import api from "../../Api/api";
 const DeleteTweet = () => {
+  const { tweetId } = useParams();
+  const navigate = useNavigate();
+
+  const handleDelete = async () => {
+    try {
+      const res = await api.delete(`/tweets/${tweetId}`);
+      console.log(res.data);
+      navigate(`/tweet`);
+    } catch (error) {
+      console.log(error, "");
+    }
+  };
   return (
     <div>
-      <h1>delete tweet</h1>
+      <h1>you really delete this tweet</h1>
+      <button onClick={handleDelete}>Delete Tweet</button>
+      <button>Cancel</button>
     </div>
   );
 };
