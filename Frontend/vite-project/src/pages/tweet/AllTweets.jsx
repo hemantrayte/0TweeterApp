f9989f4782912ec524f4737d1923e3e8d07d5b98
hemant <hemantrayte25@gmail.com>
2025-10-28 10:48:46 +0530

    Implement AllTweets component to fetch and display tweets with error handling

--- Frontend/vite-project/src/pages/tweet/AllTweets.jsx
+++ Frontend/vite-project/src/pages/tweet/AllTweets.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../../Api/api"; //
 
 const AllTweets = () => {
+  const [tweet, setTweet] = useState([]);
+  const [message, setMessage] = useState("");
+
+  const fetchAllTweets = async () => {
+    try {
+      const res = await api.get("/tweets");
+      console.log(res.data.data);
+      setTweet(res.data.data);
+      setMessage(res.data.message || "Tweets fetched successfully");
+    } catch (error) {
+      console.log(error.res?.data);
+      setMessage(error.res?.data?.message || "Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    fetchAllTweets();
+  }, []);
+
   return (
-    <div>
-      <h1>All Tweets</h1>
-    </div>
+    <>
+      <div>
+        {tweet.map((tweet) => (
+          <div key={tweet._id}>
+            <img src={tweet.image} alt="" />
+            <h1>{tweet.content}</h1>
+            <img src={tweet.owner.avatar} alt="" />
+            <h3>{tweet.owner.username}</h3>
+            <h4>{tweet.createdAt}</h4>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
