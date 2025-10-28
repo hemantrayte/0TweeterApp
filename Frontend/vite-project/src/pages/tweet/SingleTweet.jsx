7665734c12d4be395f16953a0ac2cc1f149081f5
hemant <hemantrayte25@gmail.com>
2025-10-28 13:32:38 +0530

    add singleTweet page

--- Frontend/vite-project/src/pages/tweet/SingleTweet.jsx
+++ Frontend/vite-project/src/pages/tweet/SingleTweet.jsx
@@ -1,21 +1,42 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import api from "../../Api/api";
 
 const SingleTweet = () => {
+  const { tweetId } = useParams();
 
-  const {id} = useParams()
+  const [tweet, setTweet] = useState("");
 
-  const fetchSingleTweet = async() => {
+  const fetchSingleTweet = async () => {
     try {
-      const res = await 
+      const res = await api.get(`/tweets/${tweetId}`);
+      console.log(res.data.data);
+      setTweet(res.data.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
+
+  useEffect(() => {
+    fetchSingleTweet();
+  }, []);
   return (
     <div>
-      <h1>Single tweet</h1>
+      {
+        <div>
+          <h1>{tweet.content}</h1>
+          <img src={tweet.image} alt="" />
+          <h1>{tweet?.owner?.username}</h1>
+          <img src={tweet?.owner?.avatar} alt="" />
+          <h5>{tweet.createdAt}</h5>
+          <Link to={`/tweet/update/${tweet._id}`}>
+            <button>Update tweet</button>
+          </Link>
+          <Link to={`/tweet/delete/${tweet._id}`}>
+            <button>Delete Tweet</button>
+          </Link>
+        </div>
+      }
     </div>
   );
 };
