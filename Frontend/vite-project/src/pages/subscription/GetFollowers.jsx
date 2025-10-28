f08b9f4c56eee500dd184c98362ca05136e20ca4
hemant <hemantrayte25@gmail.com>
2025-10-28 15:48:40 +0530

    defin follouser, unfollowuser, getFollowers, getFollowing page

---
+++ Frontend/vite-project/src/pages/subscription/GetFollowers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GetFollowers = () => {
+  return <div></div>;
+};
+
+export default GetFollowers;
---
+++ Frontend/vite-project/src/pages/subscription/UnfollowUser.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UnfollowUser = () => {
+  return (
+    <div>
+      <h1>UnFollo</h1>
+    </div>
+  );
+};
+
+export default UnfollowUser;
