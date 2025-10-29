2e7af6e548300bc4d5fc44f47dd8a8f80d7e7a40
hemant <hemantrayte25@gmail.com>
2025-10-29 12:28:37 +0530

    add updatecommnet page

--- Frontend/vite-project/src/pages/comments/UpdateComment.jsx
+++ Frontend/vite-project/src/pages/comments/UpdateComment.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import api from "../../Api/api";
 
 const UpdateComment = () => {
+  const [updateComment, setUpdateComment] = useState("");
+
+  const { commentId } = useParams();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await api.patch(
+        `/comments/c/${commentId}`,
+        { content: updateComment },
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+
+      console.log(res.data.data);
+      navigate(`/comment/c/${commentId}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <h1>Update Comments</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={updateComment}
+          onChange={(e) => setUpdateComment(e.target.value)}
+        />
+        <button type="submit">Update Tweet</button>
+      </form>
     </div>
   );
 };
